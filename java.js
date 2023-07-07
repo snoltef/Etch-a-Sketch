729db52d2d517302c7096a045a87ae9362e9361b
Slothgramer <santiagonolte@hotmail.com>
2023-07-07 11:39:46 -0400

    Create function getSize

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded",createBoard(16),
-    console.log("hi"));
+document.addEventListener("DOMContentLoaded",function(){
+    createBoard(16);
+    getSize();
+    console.log("hi")});
 
 function createBoard(size){
     let board = document.querySelector(".board");
     
-    board.style.gridTemplateColums = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numDivs = size *size;
@@ -13,5 +15,19 @@ function createBoard(size){
         let div = document.createElement("div");
         div.style.backgroundColor = "yellow";
         board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize(){
+    let input = prompt("Choose size of the board");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please select a number";
+    }
+    else if (input < 0 || input > 100){
+        message.innerHTML = "Please select a number between 0 and 100"   
+    }
+    else{
+        message.innerHTML = "Now you can draw!"
     }
 }
