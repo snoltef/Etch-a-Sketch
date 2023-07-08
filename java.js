95860deef3c656c3519560acb39526009886efa7
Slothgramer <santiagonolte@hotmail.com>
2023-07-07 22:07:56 -0400

    Change color for draw on and off

--- java.js
+++ java.js
@@ -1,14 +1,30 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded",function(){
     createBoard(16);
-    
+
+    document.querySelector("body").addEventListener("click",function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "Drawing mode on"
+                draw.style = "color: green"
+            }
+            else{
+                draw.innerHTML = "Drawing mode off"
+                draw.style = "color: red"
+            }
+        }
+    })  
+
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click",function(){
         let size = getSize();
         createBoard(size);
     })
-    console.log("hi")});
+})
 
 function createBoard(size){
     let board = document.querySelector(".board");
@@ -26,26 +42,30 @@ function createBoard(size){
 }
 
 function getSize(){
-    let input = prompt("Choose size of the board");
+    let input = prompt("Choose between (Biggest) 1 - 100 (Smallest)");
     let message = document.querySelector("#message");
     if(input == ""){
         message.innerHTML = "Please select a number";
     }
     else if (input < 0 || input > 100){
-        message.innerHTML = "Please select a number between 0 and 100"   
+        message.innerHTML = "Please select a number between 0 and 100" 
+        message.style.color = "red"  
     }
     else{
         message.innerHTML = "Now you can draw!"
+        message.style.color = "black"
         return input;
     }
 }
 
 function colorDiv(){
-    if (color == "random") {
+    if(click){
+        if (color == "random") {
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else{
+        }
+        else{
         this.style.backgroundColor = 'black'
+        }
     }
 }
 
@@ -56,5 +76,4 @@ function setColor(colorChoice){
 function resetBoard(){
     let divs = document.querySelectorAll("div")
     divs.forEach((div)=> div.style.backgroundColor = "white")
-
 }
